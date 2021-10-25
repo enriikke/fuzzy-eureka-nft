7fd73131258e26d27d5788ed03d2cb18a554b187
Enrique Gonzalez <enriikke@gmail.com>
2021-10-25 01:00:16 +0000

    Mint a basic NFT
    Signed-off-by: GitHub <noreply@github.com>

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const hre = require("hardhat");
+
+async function main() {
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  const Epic = await hre.ethers.getContractFactory("EpicNFT");
+  const epic = await Epic.deploy();
+
+  await epic.deployed();
+
+  console.log("Contract deployed to:", epic.address);
+
+  let txn = await epic.makeAnEpicNFT()
+  await txn.wait() // Wait for it to be mined.
+  console.log("Minted NTF")
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
