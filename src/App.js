083dd36801d49bab95c0c9eae1f23255e2eac5f2
Enrique Gonzalez <enriikke@gmail.com>
2021-10-26 16:35:36 +0000

    Add method to mint
    Signed-off-by: GitHub <noreply@github.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import epicNft from './utils/EpicNFT.json';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -60,6 +62,32 @@ const App = () => {
     }
   }
 
+  const askContractToMintNft = async () => {
+  const CONTRACT_ADDRESS = "0xd978A7CF67890CDaCAD1DeaC8Afe942CFA6d35bf";
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, epicNft.abi, signer);
+
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+}
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
@@ -73,7 +101,7 @@ const App = () => {
       );
     } else {
       return (
-        <button className="cta-button connect-wallet-button">
+        <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
           Mint NFT
         </button>
       );
